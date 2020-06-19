4742f5f6894b32d5542eb099177aafbed2442170
Shantanu Godbole <46984743+shantanugodbole@users.noreply.github.com>
2020-06-19 15:33:51 +0530

    Minor changes

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ all_words = []
 tags = []
 matcher = []
 
+# Extracting tags and sentences from intents
 for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
@@ -25,7 +26,7 @@ for intent in intents['intents']:
 all_words = refineWords(all_words)
 all_words = stemmed_output(all_words)
 
-#Unique words and tags
+# Unique words and tags
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
@@ -37,14 +38,26 @@ for (sentence, tag) in matcher:
     bag_of_words = bagofWords(sentence, all_words)
     X_train.append(bag_of_words)
 
-    label = tag.index(tag)
+    label = tags.index(tag)
     y_train.append(label)
 
+# Creating training dataset
 X_train = np.array(X_train)
 y_train = np.array(y_train)
-y_train = torch.from_numpy(y_train)
-y_train = torch.tensor(y_train, dtype=torch.long)
 
+# Checking system config
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Converting list of labels into tensors, for NN model
+y_train = torch.tensor(y_train, dtype=torch.long, device = device) 
+
+# HYPERPARAMETERS
+input_size = len(X_train[0])
+hidden_size = 10
+batch_size = 10
+output_size = len(tags)
+learning_rate = 0.0009
+epochs = 1000
 
 class ChitterChatter(Dataset):
     def __init__(self):
@@ -58,50 +71,43 @@ class ChitterChatter(Dataset):
     def __len__(self):
         return self.n_examples
 
+
 dataset = ChitterChatter()
-train_data_loader = DataLoader(dataset=dataset, batch_size=8, shuffle=True)
+train_data_loader = DataLoader(dataset, batch_size, shuffle=True)
 
-input_size = len(X_train[0])
-hidden_size = 8
-output_size = len(tags)
-learning_rate = 0.001
-epochs = 1000
-
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNetwork(input_size, hidden_size, output_size).to(device)
 
 #loss and optimization
 criteria = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 for epoch in range(epochs):
     for(words, labels) in train_data_loader:
         words = words.to(device)
         labels = labels.to(device)
 
-        #forward propogation
+        # forward propogation
         outputs = model(words)
         loss = criteria(outputs, labels)
 
-        #backward prop
+        # backward prop
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    
-    if (epoch+1) % 100 == 0 :
-        print (f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+    if (epoch+1) % 100 == 0:
+        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
 
 print(f'Final loss: {loss.item():.4f}')
 
 
 data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "hidden_size": hidden_size,
+    "output_size": output_size,
+    "all_words": all_words,
+    "tags": tags
 }
 
 FILE = "data.pth"
--- model.py
+++ model.py
@@ -1,20 +1,25 @@
-import torch 
+import torch
 import torch.nn as nn
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNetwork, self).__init__()
-        self.L1 = nn.Linear(input_size, hidden_size) 
-        self.L2 = nn.Linear(hidden_size, hidden_size) 
-        self.L3 = nn.Linear(hidden_size, num_classes)
-        self.relu  = nn.ReLU()
+        self.L1 = nn.Linear(input_size, hidden_size)
+        self.L2 = nn.Linear(hidden_size, hidden_size)
+        self.L3 = nn.Linear(hidden_size, hidden_size)
+        self.L4 = nn.Linear(hidden_size, hidden_size)
+        # self.L5 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
 
-
-    def forward(self,x):
+    def forward(self, x):
         out = self.L1(x)
         out = self.relu(out)
         out = self.L2(out)
         out = self.relu(out)
         out = self.L3(out)
+        out = self.relu(out)
+        out = self.L4(out)
+        # out = self.relu(out)
+        # out = self.L5(out)
         return out
